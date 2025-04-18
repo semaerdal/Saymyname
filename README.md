a7ec04ba5bf4afce20e833b4a7c4a091399fc4ca
Sema Erdal <95508500+semaerdal@users.noreply.github.com>
2025-04-18 21:12:55 +0200

    Ordner der Seiten eingefügt

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Saymyname
-
-run diesen command um server zu starten_
-cd say-my-name
-ng serve --open
+<!--run diesen command um server zu starten:
+//cd say-my-name
+ng serve --open-->
---
+++ say-my-name/src/app/homehomepage/homehomepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homehomepage',
+  imports: [],
+  templateUrl: './homehomepage.component.html',
+  styleUrl: './homehomepage.component.css'
+})
+export class HomehomepageComponent {
+
+}
