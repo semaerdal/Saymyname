a09ab99de6d9e95039d984c31ad1c2e7494c3b2e
Sema Erdal <95508500+semaerdal@users.noreply.github.com>
2025-05-08 17:52:45 +0200

    test footer sichtbar

--- say-my-name/src/app/app.component.ts
+++ say-my-name/src/app/app.component.ts
@@ -2,14 +2,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component'; // Add this import
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],  // Add HeaderComponent to imports
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],  // Add HeaderComponent to imports
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `
 })
 export class AppComponent {}
--- say-my-name/src/app/shared/components/footer/footer.component.ts
+++ say-my-name/src/app/shared/components/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true, 
+  imports: [RouterLink], 
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
