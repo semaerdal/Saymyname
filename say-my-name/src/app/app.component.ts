1c693b6179002aeb5ad0a0ba06f7c497758006f7
Sema Erdal <Sema.erdal@hotmail.com>
2025-06-12 01:41:30 +0200

    seite ist sichtbar

--- say-my-name/src/app/app.component.ts
+++ say-my-name/src/app/app.component.ts
@@ -1,16 +1,12 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component'; // Add this import
+import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AngularFireAnalyticsModule], 
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
