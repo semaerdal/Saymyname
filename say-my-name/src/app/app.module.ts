c8730fc8a6ac6aac30a46b1385038296b4dc77fa
tugbasahin97 <tugba.sahin@sutd.hshl.de>
2025-06-16 01:56:44 +0200

    firebase database connection

---
+++ say-my-name/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
--- say-my-name/src/environments/environment.ts
+++ say-my-name/src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000'
-  };
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAhFmdfpO6VoWQgZg_NJYCaKhUs1pRU3JI",
+    authDomain: "saymyname-8b09c.firebaseapp.com",
+    databaseURL: "https://saymyname-8b09c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "saymyname-8b09c",
+    storageBucket: "saymyname-8b09c.firebasestorage.app",
+    messagingSenderId: "240920960802",
+    appId: "1:240920960802:web:7e1347681ecb092f63cf7b",
+    measurementId: "G-TFGHDBWLYF"
+  }
+};
