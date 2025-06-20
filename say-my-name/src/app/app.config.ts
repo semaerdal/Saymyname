08bd344e913b09cf4459786d6f22e4758e567a35
tugbasahin97 <tugba.sahin@stud.hshl.de>
2025-06-21 00:38:29 +0200

    kontaktformular, infos erscheinen in firestore daten

--- say-my-name/src/app/app.config.ts
+++ say-my-name/src/app/app.config.ts
@@ -1,13 +1,12 @@
-// src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes'; 
-import { provideFirebaseApp } from '@angular/fire/app';
-import { initializeApp } from 'firebase/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([authInterceptor])
     ),
+    provideFirestore(() => getFirestore())
   ]
 };
